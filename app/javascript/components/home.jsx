a3affbe75b920e509bfa1ec7747885f79c12b0fb
donrestarone <shashikejayatunge@gmail.com>
2024-07-22 14:56:04 -0400

    swap out bootstrap styles for tailwind

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -22,7 +22,7 @@ const DisplayPosts = () => {
   if (error) return <div>An error occurred: {error.message}</div>;
 
   return (
-    <ol>
+    <ol className="list-disc">
       {posts.map((post) => (
         <li key={post.id}>{post.title}</li>
       ))}
@@ -31,26 +31,8 @@ const DisplayPosts = () => {
 };
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Food Recipes!!!!</h1>
-        <p className="lead">
-          A curated list of recipes for the best homemade meal and delicacies.
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/recipes"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          View Recipes
-        </Link>
-        <div>
-          <p>Posts</p>
-          {DisplayPosts()}
-        </div>
-      </div>
-    </div>
+  <div className="container mx-auto px-5">
+    <p className="text-3xl font-bold underline">Posts</p>
+    {DisplayPosts()}
   </div>
 );
